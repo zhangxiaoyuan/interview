c16018f34b13f0fb5cb3f3c95f44e913f5f2bc38
张小猿 <624518625@qq.com>
2016-08-09 18:01:23 +0800

    Update 5_printReverseList.cpp

--- src/point_to_offer/5_printReverseList.cpp
+++ src/point_to_offer/5_printReverseList.cpp
@@ -2,8 +2,9 @@
 /*
 给定一个链表，输入链表头结点，从尾到头反向打印链表中的每个节点的值
 1.采用std的栈存储每一个节点，然后就可以发现打印出来
-2.采用一个新的链表，将链表反序
-3.直接修改原来的链表，将原来的链表反序
+2.采用递归实现，在打印每一个节点之前，先打印他的next节点
+3.采用一个新的链表，将链表反序
+4.直接修改原来的链表，将原来的链表反序
 */
 typedef struct _List_Node
 {
@@ -34,6 +35,23 @@ int printReverseList(listNode* pHead)
 		cout <<" " << node->data<< endl;
 		nodeStack.pop();
 	}
+
+	return 0;
+}
+
+int printReverseList1(listNode* pHead)	
+{
+	if (NULL == pHead)
+	{
+		return -1;
+	}
+	 
+	if (pHead->pNext == NULL)
+	{
+		cout << " " << pHead->data << endl;
+	}
+
+	printReverseList1(pHead->pNext);
 
 	return 0;
 }
