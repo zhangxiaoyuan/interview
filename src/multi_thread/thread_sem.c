06044fe5e5fc94b3159786f329b163905436a205
张小猿 <624518625@qq.com>
2016-09-23 10:18:46 +0800

    Update thread_sem.c

--- src/multi_thread/thread_sem.c
+++ src/multi_thread/thread_sem.c
@@ -1,3 +1,16 @@
+/*
+如同进程一样，线程也可以通过信号量来实现通信，虽然是轻量级的。信号量函数的名字都以"sem_"打头。线程使用的基本信号量函数有四个。
+信号量初始化。
+int sem_init (sem_t *sem , int pshared, unsigned int value);
+这是对由sem指定的信号量进行初始化，设置好它的共享选项(linux 只支持为0，即表示它是当前进程的局部信号量)，然后给它一个初始值VALUE。
+等待信号量。给信号量减1，然后等待直到信号量的值大于0。
+int sem_wait(sem_t *sem);
+释放信号量。信号量值加1。并通知其他等待线程。
+int sem_post(sem_t *sem);
+销毁信号量。我们用完信号量后都它进行清理。归还占有的一切资源。
+int sem_destroy(sem_t *sem);
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
