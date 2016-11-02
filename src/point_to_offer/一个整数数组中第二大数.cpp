3f35cdf4964890b2edaba13af18ab67da9693cff
张小猿 <624518625@qq.com>
2016-11-02 10:43:25 +0800

    Create 一个整数数组中第二大数.cpp

---
+++ src/point_to_offer/一个整数数组中第二大数.cpp
@@ -0,0 +1,22 @@
+const int MINNUMBER = -32767 ;
+int find_sec_max( int data[] , int count) //类似于1 4 4 4这样的序列将认为1是第二大数
+{
+    int maxnumber = data[0] ;
+    int sec_max = MINNUMBER ;
+    
+    for ( int i = 1 ; i < count ; i++)
+    {
+        if ( data[i] > maxnumber )
+        {
+            sec_max = maxnumber ;
+            maxnumber = data[i] ;
+        }
+        else
+        {
+           if ( data[i] > sec_max )
+                sec_max = data[i] ;
+        }
+    }
+    
+    return sec_max ;
+}
